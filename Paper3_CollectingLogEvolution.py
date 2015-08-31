d0e7a42a3d2857266a266d291b30af918b329743
suhas kabinna <suhas.servesh@gmail.com>
2015-08-31 09:05:01 -0400

    3

--- Paper3_CollectingLogEvolution.py
+++ Paper3_CollectingLogEvolution.py
@@ -1027,7 +1027,7 @@ def GatherMetricsForNotChangedLogs(addedLog,deletedLog,metricsNeeded,allCodeChur
                         blockLine = aes
                         countBlock = 0
                         while not re.match('^@@.*',list1[blockLine].logLine) or countBlock < 10:
-                            if re.match('^+.*',list1[blockLine].logLine) or re.match('\s.*',list1[blockLine].logLine):
+                            if re.match('^+.*',list1[blockLine].logLine):
                                 metricsNeeded.AddedCodeBlock = metricsNeeded.AddedCodeBlock + list1[blockLine].logLine
                                 print list1[blockLine].logLine
                                 blockLine = blockLine - 1
