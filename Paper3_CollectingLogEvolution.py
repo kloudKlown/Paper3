38b551b469d10094fca8c111158b706a1d673c72
suhas kabinna <suhas.servesh@gmail.com>
2015-09-01 12:57:20 -0400

    3

--- Paper3_CollectingLogEvolution.py
+++ Paper3_CollectingLogEvolution.py
@@ -700,7 +700,7 @@ def StaticTextCheck(addedLog,deletedLog,allCodeChurn,metricsNeeded,commit_added)
         # print the variables now
         for a in variablesdel.splitlines():
             ### First is simple thing to remove + marks
-            a.replace('+','')
+            a = a.translate('+','')
             print a
 
         ######## Find levenshtein of static parts
