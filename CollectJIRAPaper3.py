6a5eefeeef11f7637bf2d38a6362a640952a0212
suhas kabinna <suhas.servesh@gmail.com>
2015-09-14 19:46:37 -0400

    1

--- CollectJIRAPaper3.py
+++ CollectJIRAPaper3.py
@@ -8,10 +8,38 @@ import commands
 
 file1 = open('AllFiles1','r')
 
+DirectList = ""
+
+InDirectList = ""
+
 for l in file1:
-	print l
-	l = l.strip('\n')
-	p = ""
-	p = commands.getoutput('grep -i \'switch.*slf4j\' %s'  %(l))
-	if p:
-		print p
+#       print l
+        l = l.strip('\n')
+        p = ""
+        p = commands.getoutput('grep -i \'switch.*slf4j\' %s'  %(l))
+        #print p
+        if p:
+                DirectList =DirectList + l + '\n'
+                print l
+
+        p = commands.getoutput('grep -i \'switch.*log4j\' %s' %(l))
+        if p:
+			DirectList =DirectList + l + '\n'
+			print l
+
+        p = commands.getoutput('grep -i \'switch.*logback\' %s' %(l))
+        if p:
+			DirectList =DirectList + l + '\n'
+			print l
+
+			
+        p = commands.getoutput('grep -i \'switch.*logging\' %s' %(l))
+        if p:
+			DirectList =DirectList + l + '\n'
+			print l
+			
+        p = commands.getoutput('grep -i \'switch.*jcl\' %s' %(l))
+        if p:
+			DirectList =DirectList + l + '\n'
+			print l
+
